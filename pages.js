8ebaf14ca3ec9e650359fc3f28e9fcf8c80e5d24
ricardopresto <ricardopresto5@gmail.com>
2019-08-24 18:10:48 +0200

    added home links

--- pages.js
+++ pages.js
@@ -1,7 +1,19 @@
-const aboutPage = () => {};
+const aboutPage = () => {
+  const template = `<h1 id="arse">Arseface</h1>
+  <div>hello hello hello hello hello hello hello hello hello hello hello hello hello hello 
+  hello hello hello hello hello hello hello hello hello hello hello hello hello hello</div>`;
+  main.innerHTML = template;
+};
 
-const projectsPage = () => {};
+const projectsPage = () => {
+  main.innerHTML = `<a href="./weather_app/index.html" >Weather</a>
+  <a href="./mandelbrot/index.html" >Mandelbrot</a>
+  <a href="./bounce/index.html" >Bounce</a>
+  <a href="./gallery/index.html" >Gallery</a>`;
+};
 
-const contactPage = () => {};
+const contactPage = () => {
+  main.innerHTML = `<h1>GO AWAY</h1>`;
+};
 
 export { aboutPage, projectsPage, contactPage };
