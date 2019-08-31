4a5c02294b839e4fde1d533ead602a97d321f233
ricardopresto <ricardopresto5@gmail.com>
2019-08-31 16:25:50 +0200

    added code links

--- pages.js
+++ pages.js
@@ -8,25 +8,32 @@ const aboutPage = () => {
 const projectsPage = () => {
   main.innerHTML = `
   <div id="projectContainer">
-  
+
     <div id="weatherApp" class="projectBox">
-    <img src="weather.jpg" />
+    <img src="./images/weather.jpg" />
     <a href="./weather_app/index.html" >Weather</a>
+    <a href="https://github.com/ricardopresto/website2/tree/master/weather_app"  target="_blank" >Code</a>
     </div>
 
     <div id="mandelbrotApp" class="projectBox">
-    <img src="mandelbrot.jpg" />
+    <img src="./images/mandelbrot.jpg" />
     <a href="./mandelbrot/index.html" >Mandelbrot</a>
+    <a href="https://github.com/ricardopresto/website2/tree/master/mandelbrot"  target="_blank" >Code</a>
+
     </div> 
 
     <div id="bounceApp" class="projectBox">
-    <img src="bounce.jpg" />
+    <img src="./images/bounce.jpg" />
     <a href="./bounce/index.html" >Bounce</a>
+    <a href="https://github.com/ricardopresto/website2/tree/master/bounce"  target="_blank" >Code</a>
+
     </div>
 
     <div id="galleryApp" class="projectBox">
-    <img src="gallery.jpg" />
+    <img src="./images/gallery.jpg" />
     <a href="./gallery/index.html" >Gallery</a>
+    <a href="https://github.com/ricardopresto/website2/tree/master/gallery"  target="_blank" >Code</a>
+
     </div> 
 
   </div>
