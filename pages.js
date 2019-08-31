ab957daf215ff13a187cc511fd229429b3a31396
ricardopresto <ricardopresto5@gmail.com>
2019-08-31 21:20:31 +0200

    added 'about' section text

--- pages.js
+++ pages.js
@@ -1,10 +1,16 @@
 const aboutPage = () => {
-  main.innerHTML = `<h1 id="arse">About</h1>
-  <div>about about about about about about about about about 
-  about about about about about about about about about about 
-  about about about about about about about about about about 
-  about about about about about about about about about about 
-  about about about about about about about about about about 
+  main.innerHTML = `
+  <div id="aboutContainer">
+  <div>I started to teach myself web development at the end of 2018, looking for a change
+  of career. Before that, programming was just a hobby, beginning with the ZX Spectrum in 
+  the early 1980s. I love the feeling that anything is possible with software, and am 
+  happiest when I have a problem to solve.</div>
+  <div>My university degree is in philosophy, which could be described as 'conceptual 
+  engineering'. I find that the precision and logical analysis that philosophy teaches is
+  frequently applicable to the process of writing software.</div>
+  <div>I'm currently looking for my first web development job, so I don't have industry
+  experience, but I'm highly motivated and a fast learner. I'm currrently studying React 
+  and Vue in my spare time and enjoying it very much.</div>
   </div>`;
 
   about.style.color = "#fff";
@@ -22,7 +28,7 @@ const projectsPage = () => {
       </div>
       <div class="linksBox">
         <a href="./weather_app/index.html" target="_blank" >Webpage</a>
-        <a href="https://github.com/ricardopresto/website2/tree/master/weather_app" target="_blank" >Code</a>
+        <a href="https://github.com/ricardopresto/website/tree/master/weather_app" target="_blank" >Code</a>
       </div>
     </div>
     <div id="mandelbrotApp" class="projectBox">
@@ -32,7 +38,7 @@ const projectsPage = () => {
       </div>
       <div class="linksBox">
         <a href="./mandelbrot/index.html" target="_blank" >Webpage</a>
-        <a href="https://github.com/ricardopresto/website2/tree/master/mandelbrot" target="_blank" >Code</a>
+        <a href="https://github.com/ricardopresto/website/tree/master/mandelbrot" target="_blank" >Code</a>
       </div>
     </div> 
     <div id="bounceApp" class="projectBox">
@@ -42,7 +48,7 @@ const projectsPage = () => {
       </div>
       <div class="linksBox">
         <a href="./bounce/index.html" target="_blank" >Webpage</a>
-        <a href="https://github.com/ricardopresto/website2/tree/master/bounce" target="_blank" >Code</a>
+        <a href="https://github.com/ricardopresto/website/tree/master/bounce" target="_blank" >Code</a>
       </div>
     </div>
     <div id="galleryApp" class="projectBox">
@@ -52,7 +58,7 @@ const projectsPage = () => {
       </div>
       <div class="linksBox">
         <a href="./gallery/index.html" target="_blank" >Webpage</a>
-        <a href="https://github.com/ricardopresto/website2/tree/master/gallery" target="_blank" >Code</a>
+        <a href="https://github.com/ricardopresto/website/tree/master/gallery" target="_blank" >Code</a>
       </div>
     </div> 
   </div>`;
