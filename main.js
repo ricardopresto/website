7bb62801c311af699570df2cc1b0ee4d8c249bae
ricardopresto <ricardopresto5@gmail.com>
2019-08-31 18:11:36 +0200

    page link color change

--- main.js
+++ main.js
@@ -4,7 +4,49 @@ const main = document.getElementById("main");
 const about = document.getElementById("about");
 const projects = document.getElementById("projects");
 const contact = document.getElementById("contact");
+let activePage = "about";
 
-about.addEventListener("click", aboutPage);
-projects.addEventListener("click", projectsPage);
-contact.addEventListener("click", contactPage);
+aboutPage();
+
+const aboutPageClick = () => {
+  activePage = "about";
+  aboutPage();
+};
+
+const projectsPageClick = () => {
+  activePage = "projects";
+  projectsPage();
+};
+
+const contactPageClick = () => {
+  activePage = "contact";
+  contactPage();
+};
+
+about.addEventListener("click", aboutPageClick);
+projects.addEventListener("click", projectsPageClick);
+contact.addEventListener("click", contactPageClick);
+
+about.onmouseover = function() {
+  this.style.color = "#fff";
+};
+
+about.onmouseout = function() {
+  activePage == "about" ? null : (this.style.color = "darkslategray");
+};
+
+projects.onmouseover = function() {
+  this.style.color = "#fff";
+};
+
+projects.onmouseout = function() {
+  activePage == "projects" ? null : (this.style.color = "darkslategray");
+};
+
+contact.onmouseover = function() {
+  this.style.color = "#fff";
+};
+
+contact.onmouseout = function() {
+  activePage == "contact" ? null : (this.style.color = "darkslategray");
+};
--- pages.js
+++ pages.js
@@ -1,47 +1,69 @@
 const aboutPage = () => {
-  const template = `<h1 id="arse">Arseface</h1>
+  main.innerHTML = `<h1 id="arse">Arseface</h1>
   <div>hello hello hello hello hello hello hello hello hello hello hello hello hello hello 
   hello hello hello hello hello hello hello hello hello hello hello hello hello hello</div>`;
-  main.innerHTML = template;
+
+  about.style.color = "#fff";
+  projects.style.color = "darkslategray";
+  contact.style.color = "darkslategray";
 };
 
 const projectsPage = () => {
   main.innerHTML = `
   <div id="projectContainer">
-
     <div id="weatherApp" class="projectBox">
-    <img src="./images/weather.jpg" />
-    <a href="./weather_app/index.html" >Weather</a>
-    <a href="https://github.com/ricardopresto/website2/tree/master/weather_app"  target="_blank" >Code</a>
+      <img src="./images/weather.jpg" />
+      <div class="description">
+      A front-end for the OpenWeatherMap API
+      </div>
+      <div class="linksBox">
+        <a href="./weather_app/index.html" target="_blank" >Webpage</a>
+        <a href="https://github.com/ricardopresto/website2/tree/master/weather_app" target="_blank" >Code</a>
+      </div>
     </div>
-
     <div id="mandelbrotApp" class="projectBox">
-    <img src="./images/mandelbrot.jpg" />
-    <a href="./mandelbrot/index.html" >Mandelbrot</a>
-    <a href="https://github.com/ricardopresto/website2/tree/master/mandelbrot"  target="_blank" >Code</a>
-
+      <img src="./images/mandelbrot.jpg" />
+      <div class="description">
+      A Mandelbrot Set generator
+      </div>
+      <div class="linksBox">
+        <a href="./mandelbrot/index.html" target="_blank" >Webpage</a>
+        <a href="https://github.com/ricardopresto/website2/tree/master/mandelbrot" target="_blank" >Code</a>
+      </div>
     </div> 
-
     <div id="bounceApp" class="projectBox">
-    <img src="./images/bounce.jpg" />
-    <a href="./bounce/index.html" >Bounce</a>
-    <a href="https://github.com/ricardopresto/website2/tree/master/bounce"  target="_blank" >Code</a>
-
+      <img src="./images/bounce.jpg" />
+      <div class="description">
+      A traditional old-school Breakout game
+      </div>
+      <div class="linksBox">
+        <a href="./bounce/index.html" target="_blank" >Webpage</a>
+        <a href="https://github.com/ricardopresto/website2/tree/master/bounce" target="_blank" >Code</a>
+      </div>
     </div>
-
     <div id="galleryApp" class="projectBox">
-    <img src="./images/gallery.jpg" />
-    <a href="./gallery/index.html" >Gallery</a>
-    <a href="https://github.com/ricardopresto/website2/tree/master/gallery"  target="_blank" >Code</a>
-
+      <img src="./images/gallery.jpg" />
+      <div class="description">
+      A simple image gallery app
+      </div>
+      <div class="linksBox">
+        <a href="./gallery/index.html" target="_blank" >Webpage</a>
+        <a href="https://github.com/ricardopresto/website2/tree/master/gallery" target="_blank" >Code</a>
+      </div>
     </div> 
+  </div>`;
 
-  </div>
-  `;
+  about.style.color = "darkslategray";
+  projects.style.color = "#fff";
+  contact.style.color = "darkslategray";
 };
 
 const contactPage = () => {
   main.innerHTML = `<h1>GO AWAY</h1>`;
+
+  about.style.color = "darkslategray";
+  projects.style.color = "darkslategray";
+  contact.style.color = "#fff";
 };
 
 export { aboutPage, projectsPage, contactPage };
